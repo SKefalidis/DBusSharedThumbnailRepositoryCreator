39a6c94518d5844598ec3dcb72de80bc4a214b8c
Sergios - Anestis Kefalidis <megistios@gmail.com>
2021-09-27 13:27:00 +0300

    Comments and variable renaming

--- sharedx.sh
+++ sharedx.sh
@@ -1,79 +1,93 @@
 #!/bin/bash
+
+# Notes:
+# - local thumbnails are the thumbnails stored in the standard thumbnail location of the system
+# - shared thumbnails are the ones placed in Shared Thumbnail Repositories
+# - this script was created and tested for Thunar and Tumbler, different file-manager/thumbnailer
+# combos might require some small changes regarding the exact form of the paths/uris given but 
+# the main logic should be the same 
+
 directory=$1
 if [ ! -e $directory ]; then
     echo "Directory not found"
     exit 2
 fi
 
-currentDirectory=$(pwd)
-directoryPath="$currentDirectory/$directory"
-
 if [ -z "$XDG_CACHE_HOME" ]
 then
-	thumbnailsPath="$HOME/.cache/"
+	localThumbnailsRepo="$HOME/.cache/"
 else
-	thumbnailsPath="$XDG_CACHE_HOME/"
+	localThumbnailsRepo="$XDG_CACHE_HOME/"
 fi
 
-thumbnailsPath+="thumbnails/"
-normalThumbnailsPath="${thumbnailsPath}normal/"
-largeThumbnailsPath="${thumbnailsPath}large/"
+localThumbnailsRepo+="thumbnails/"
+localNormalFolder="${localThumbnailsRepo}normal/"
+localLargeFolder="${localThumbnailsRepo}large/"
 
-IFS=$'\n' # change separator to newline
-for file in $(find $directory -name "*.jpg"); do	
-	# important location
+# change separator to newline to iterate through filepaths correctly
+IFS=$'\n'
+
+# recursively create shared thumbnail repositories
+for file in $(find $directory -name "*.jpg"); do # TODO: Support more filetypes
 	filePath=$(realpath "$file")
 	fileDir=$(dirname "$filePath")
-	uri="file://$filePath"
-	md5_uri=`echo -n "$uri" | md5sum | cut -d" " -f1`
-	filePathEncoded=$(python3 -c "import sys, pathlib; print(pathlib.Path(input()).resolve().as_uri())" <<< $filePath)
-	md5_shared=`echo -n "$(basename $filePathEncoded)" | md5sum | cut -d" " -f1`
+
+	# used request the creation of a local thumbnail
+	#
+	fakeUri="file://$filePath"
+	md5FakeUri=`echo -n "$fakeUri" | md5sum | cut -d" " -f1`
+
+	# used to copy the local thumbnail to a shared repository
+	#
+	realUri=$(python3 -c "import sys, pathlib; print(pathlib.Path(input()).resolve().as_uri())" <<< $fakeUri) # encode special chars like ' '
+	md5Shared=`echo -n "$(basename $realUri)" | md5sum | cut -d" " -f1`
 
 	# shared locations
-	sharedRepo="${fileDir}/.sh_thumbnails/"
-	sharedNormalFolder="${sharedRepo}normal/"
-	sharedLargeFolder="${sharedRepo}large/"
-	sharedNormalThumbnail="${sharedNormalFolder}${md5_shared}.png"
-	sharedLargeThumbnail="${sharedLargeFolder}${md5_shared}.png"
+	#
+	sharedThumbnailsRepo="${fileDir}/.sh_thumbnails/"
+	sharedNormalFolder="${sharedThumbnailsRepo}normal/"
+	sharedLargeFolder="${sharedThumbnailsRepo}large/"
+	sharedNormalThumbnail="${sharedNormalFolder}${md5Shared}.png"
+	sharedLargeThumbnail="${sharedLargeFolder}${md5Shared}.png"
 
     
 	# If a shared "Normal"-size thumbnail does not exist then create one.
 	#
-	# 1. Request the creation of a "Normal"-size thumbnail by the dbus thumbnailer.
-	# 2. Wait until the thumbnail is created.
-	# 3. Copy that thumbnail to the proper Shared Thumbnail Repository.
 	if [ ! -e $sharedNormalThumbnail ]; then
-		normal="${normalThumbnailsPath}${md5_uri}.png"
+		# 1. Request the creation of a "Normal"-size thumbnail by the dbus thumbnailer.
+		normal="${localNormalFolder}${md5FakeUri}.png"
 		if [ ! -e $normal ]; then
-			cmd="dbus-send --session --print-reply --dest=org.freedesktop.thumbnails.Thumbnailer1 /org/freedesktop/thumbnails/Thumbnailer1 org.freedesktop.thumbnails.Thumbnailer1.Queue array:string:\"$uri\" array:string:\"image/png\" string:\"normal\" string:\"default\" uint32:0"
+			cmd="dbus-send --session --print-reply --dest=org.freedesktop.thumbnails.Thumbnailer1 /org/freedesktop/thumbnails/Thumbnailer1 org.freedesktop.thumbnails.Thumbnailer1.Queue array:string:\"$fakeUri\" array:string:\"image/png\" string:\"normal\" string:\"default\" uint32:0"
 			eval $cmd
 		fi
+		# 2. Wait until the thumbnail is created.
 		while [ ! -e $normal ]
 		do
-			echo "sleep1 $normal"
+			# echo "sleep1 $normal"
 			$(sleep 0.05)
 		done
-
+		# 3. Copy that thumbnail to the proper Shared Thumbnail Repository.
 		$(mkdir -p $sharedNormalFolder)
 		$(cp $normal $sharedNormalThumbnail)
-		echo "copy $normal to $sharedNormalThumbnail"
+		# echo "copy $normal to $sharedNormalThumbnail"
 	fi
     
 	# Do the same for "Large"-size thumbnails
+	#
 	if [ ! -e $sharedLargeThumbnail ]; then
-		large="${largeThumbnailsPath}${md5_uri}.png"
+		large="${localLargeFolder}${md5FakeUri}.png"
 		if [ ! -e $large ]; then
-			cmd="dbus-send --session --print-reply --dest=org.freedesktop.thumbnails.Thumbnailer1 /org/freedesktop/thumbnails/Thumbnailer1 org.freedesktop.thumbnails.Thumbnailer1.Queue array:string:\"$uri\" array:string:\"image/png\" string:\"large\" string:\"default\" uint32:0"
+			cmd="dbus-send --session --print-reply --dest=org.freedesktop.thumbnails.Thumbnailer1 /org/freedesktop/thumbnails/Thumbnailer1 org.freedesktop.thumbnails.Thumbnailer1.Queue array:string:\"$fakeUri\" array:string:\"image/png\" string:\"large\" string:\"default\" uint32:0"
 			eval $cmd
 		fi
 		while [ ! -e $large ]
 		do
-			echo "sleep2"
+			# echo "sleep2"
 			$(sleep 0.05)
 		done
 		
 		$(mkdir -p $sharedLargeFolder)
 		$(cp $large $sharedLargeThumbnail)
-		echo "copy $large to $sharedLargeThumbnail"
+		# echo "copy $large to $sharedLargeThumbnail"
 	fi
 done
